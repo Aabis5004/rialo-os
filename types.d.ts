976fff49535986c423cb591222faaf63c3c9d92d
Aabis5004 <lonearif5004@gmail.com>
2026-07-11 13:54:56 +0530

    fix force-graph types

--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,7 @@
 declare module "three-spritetext";
-declare module "react-force-graph-3d";
+
+declare module "react-force-graph-3d" {
+  import { ComponentType } from "react";
+  const ForceGraph3D: ComponentType<any>;
+  export default ForceGraph3D;
+}
